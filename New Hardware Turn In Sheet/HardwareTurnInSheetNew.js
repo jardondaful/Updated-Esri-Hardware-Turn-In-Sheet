3754a57fc8542e124c981b9cbf7df793b2314349
Jordan Jonathan Lin <68207907+jardondaful@users.noreply.github.com>
2023-09-05 09:56:09 -0700

    Update HardwareTurnInSheetNew.js

--- New Hardware Turn In Sheet/HardwareTurnInSheetNew.js
+++ New Hardware Turn In Sheet/HardwareTurnInSheetNew.js
@@ -1,14 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('myForm');
-  
-    form.addEventListener('submit', function (event) {
-      event.preventDefault();
-  
-      const name = form.querySelector('#name').value;
-      const email = form.querySelector('#email').value;
-      const gender = form.querySelector('#gender').value;
-  
-      console.log('Form Data:', { name, email, gender });
-    });
+document.addEventListener("DOMContentLoaded", function() {
+  // Function to toggle sections based on action type
+  document.getElementById("actionType").addEventListener("change", function() {
+      var selectedValue = this.value;
+      var separationSection = document.getElementById("separationSection");
+      var hardwareTurnInSection = document.getElementById("hardwareTurnInSection");
+
+      if (selectedValue === "separation") {
+          separationSection.style.display = "block";
+          hardwareTurnInSection.style.display = "none";
+      } else if (selectedValue === "hardwareTurnIn") {
+          separationSection.style.display = "none";
+          hardwareTurnInSection.style.display = "block";
+      }
   });
-  
+
+  // Function to handle form submissions
+  document.getElementById("separationForm").addEventListener("submit", function(event) {
+      // Validate form fields here
+      // Perform submission or show error messages
+      
+      // For example, you might prevent the default form submission for this example
+      event.preventDefault();
+  });
+
+  // Function to handle hardware turn-in form submissions
+  // ... similar to the above function for separation form
+
+  // Other functions for dynamic behavior or interactions go here
+});
